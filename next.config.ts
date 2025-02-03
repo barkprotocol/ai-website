e0c8d1642a9f76a9a136198a1487ea407decfd1e
BARK Protocol <177503488+barkprotocol@users.noreply.github.com>
2025-02-03 12:14:48 +0000

    change configs

---
+++ next.config.ts
@@ -0,0 +1,30 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["ucarecdn.com", "cryptologos.cc"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        stream: require.resolve("stream-browserify"),
+        crypto: require.resolve("crypto-browserify"),
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
